13dd3452d7848abcf828ea2a2c3ac70562dadbda
Sam Acuna <samueleishion@gmail.com>
2014-03-03 15:23:24 -0600

    Added explosions to asteroids and player

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,8 +3,18 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour {
 
+	public GameObject explosion; 
+	public GameObject playerExplosion; 
+
 	void OnTriggerEnter(Collider other) {
 		if(other.tag=="Boundary") return; 
+
+		// explode 
+		Instantiate(explosion, transform.position, transform.rotation); 
+		if(other.tag=="Player") 
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation); 
+
+		// mark objects for deletion 
 		Destroy(other.gameObject); 
 		Destroy(gameObject); 
 	} 
