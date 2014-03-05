828cc9d4a15c651d7343d5de20223a5dc0a5be48
Sam Acuna <samueleishion@gmail.com>
2014-03-05 11:40:47 -0600

    Added music and SFX

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,6 +21,7 @@ public class PlayerController : MonoBehaviour {
 		if(Input.GetButton ("Fire1") && Time.time > nextFire) { 
 			nextFire = Time.time + fireRate; 
 			Instantiate(shot, shotSpawn.position, shotSpawn.rotation); 
+			audio.Play (); 
 		}
 	}
 
